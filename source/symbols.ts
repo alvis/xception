70f5661f7718e0a55a5ac3320ffb057be74ef6c5
Alvis HT Tang <alvis@hilbert.space>
2023-10-18 02:12:58 +0100

    feat: provide symbols to be used for accessing private properties

---
+++ source/symbols.ts
@@ -0,0 +1,18 @@
+/*
+ *                            *** MIT LICENSE ***
+ * -------------------------------------------------------------------------
+ * This code may be modified and distributed under the MIT license.
+ * See the LICENSE file for details.
+ * -------------------------------------------------------------------------
+ *
+ * @summary   Symbols used in the prototype
+ * @author    Alvis HT Tang <alvis@hilbert.space>
+ * @license   MIT
+ * @copyright Copyright (c) 2020 - All Rights Reserved.
+ * -------------------------------------------------------------------------
+ */
+
+export const $namespace = Symbol('namespace');
+export const $tags = Symbol('tags');
+export const $cause = Symbol('cause');
+export const $meta = Symbol('meta');
