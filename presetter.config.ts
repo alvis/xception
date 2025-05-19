4c161e2374b49e0a64146ea00e4066c4ebcaa586
Alvis HT Tang <alvis@hilbert.space>
2025-05-19 17:16:38 +0100

    chore: speedup eslint

--- presetter.config.ts
+++ presetter.config.ts
@@ -6,4 +6,18 @@ import { name } from './package.json';
 
 export default preset(name, {
   extends: [esm, strict],
+  override: {
+    assets: {
+      'eslint.config.ts': {
+        default: [
+          {
+            name: 'xception:override',
+            rules: {
+              'sonarjs/no-commented-code': 'off', // takes too long to run
+            },
+          },
+        ],
+      },
+    },
+  },
 });
